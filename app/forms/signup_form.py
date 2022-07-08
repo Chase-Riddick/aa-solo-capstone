98152de47e5aa8efebe3a3b0e337189901d21518
Chase Riddick <chaseriddick@outlook.com>
2022-07-08 07:15:27 -0700

    Additional Signup validation descriptors added

--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField
-from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms.validators import DataRequired, Email, ValidationError, Length
 from app.models import User
 
 
@@ -9,7 +9,15 @@ def user_exists(form, field):
     email = field.data
     user = User.query.filter(User.email == email).first()
     if user:
-        raise ValidationError('Email address is already in use.')
+        raise ValidationError('This email address is already in use.')
+
+def validate_email(form, field):
+    email = field.data
+    if len(email) < 5 or len(email) > 20:
+        raise ValidationError('The email address must be between 5 and 20 characters long.')
+    if not '@' in email:
+        raise ValidationError("The email address must contain an '@' symbol.")
+    return email
 
 
 def username_exists(form, field):
@@ -17,11 +25,11 @@ def username_exists(form, field):
     username = field.data
     user = User.query.filter(User.username == username).first()
     if user:
-        raise ValidationError('Username is already in use.')
+        raise ValidationError('This username is already in use. Please choose another.')
 
 
 class SignUpForm(FlaskForm):
     username = StringField(
-        'username', validators=[DataRequired(message = "A username is required."), username_exists])
-    email = StringField('email', validators=[DataRequired(message = "An email is required."), user_exists])
-    password = StringField('password', validators=[DataRequired(message = "A password is required.")])
+        'username', validators=[DataRequired(message = "A username is required."), username_exists, Length(min=3, max=15, message="The username must be between 3 and 15 characters long.")])
+    email = StringField('email', validators=[DataRequired(message = "An email is required."), user_exists, validate_email])
+    password = StringField('password', validators=[DataRequired(message = "A password is required."), Length(min=5, message="The password must be at least five characters long.")])
