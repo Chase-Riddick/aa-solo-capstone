58be674f5b9ae3d61dafef0b4fcb54275f062acc
Chase Riddick <chaseriddick@outlook.com>
2022-07-09 14:42:58 -0700

    trying different api key

--- react-app/src/components/MapTest/index.js
+++ react-app/src/components/MapTest/index.js
@@ -12,7 +12,7 @@ import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-map
 
 const MyMapComponent = compose(
   withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyBJfQUfetiFlIWMxEuXalSMUtkkghhJspE&v=3.exp&libraries=geometry,drawing,places",
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyCV1aH4qqDr2uUEG4I9FKeB6scau4FWuWw&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
