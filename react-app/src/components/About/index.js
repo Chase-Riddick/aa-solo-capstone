35105b0996ec6c0bb6d456090b6b0887080bd0cb
Chase Riddick <chaseriddick@outlook.com>
2022-07-14 14:54:42 -0700

    about image added

--- react-app/src/components/About/index.js
+++ react-app/src/components/About/index.js
@@ -5,7 +5,7 @@ export default function About () {
     return (
         <div className='about-page-container'>
             <div className='about-container'>
-                <img className='developer-img' src='/images/default-profile-img.png' />
+                <img className='developer-img' src='https://localcatches.s3.us-west-2.amazonaws.com/Screen+Shot+2022-07-14+at+14.42.43.png' />
                 <div className='personal-slogan'>Chase Riddick</div>
                 <div className='icons'>Github --- Linkedin</div>
             </div>
